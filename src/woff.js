3de5a25bfbc218fe14d7d2195dc71f3f0a191035
Ruben Maher <ruben@maher.fyi>
2018-08-11 09:09:58 +0930

    Run prettier

--- src/woff.js
+++ src/woff.js
@@ -1,5 +1,3 @@
-
-
 /**
  * @module woff
  * @namespace
--- test/io.js
+++ test/io.js
@@ -1,5 +1,3 @@
-
-
 const basePath = './node_modules/font-awesome/fonts';
 const fs = require('fs');
 const path = require('path');
@@ -11,23 +9,26 @@ const magic = {
   // http://www.garykessler.net/library/file_sigs.html
   ttf: [0x00, 0x01, 0x00, 0x00],
   // "wOFF", see https://www.w3.org/TR/WOFF/#WOFFHeader
-  woff: [0x77, 0x4f, 0x46, 0x46]
+  woff: [0x77, 0x4f, 0x46, 0x46],
 };
 
-
-test('Decode WOFF data.', (t) => {
+test('Decode WOFF data.', t => {
   temp.mkdir('node-sfnt2woff-zopfli', (err, dirPath) => {
     if (err) throw err;
-    const data = fs.readFileSync(path.join(basePath, 'fontawesome-webfont.woff'));
+    const data = fs.readFileSync(
+      path.join(basePath, 'fontawesome-webfont.woff')
+    );
     const file = path.join(dirPath, 'decoded.ttf');
     // eslint-disable-next-line no-shadow
-    fs.writeFile(file, woff.decode(data), (err) => {
+    fs.writeFile(file, woff.decode(data), err => {
       if (err) throw err;
       const buffer = new Buffer(4);
       // eslint-disable-next-line no-shadow
-      fs.open(file, 'r', (err, fd) => { // eslint-disable-next-line no-shadow
+      fs.open(file, 'r', (err, fd) => {
+        // eslint-disable-next-line no-shadow
         fs.read(fd, buffer, 0, 4, 0, (err, bytesRead, buffer) => {
-          fs.close(fd, (err) => { // eslint-disable-line no-shadow
+          fs.close(fd, err => {
+            // eslint-disable-line no-shadow
             if (err) throw err;
             t.ok(buffer.equals(new Buffer(magic.ttf)));
             t.end();
@@ -38,19 +39,23 @@ test('Decode WOFF data.', (t) => {
   });
 });
 
-test('Encode WOFF data.', (t) => {
+test('Encode WOFF data.', t => {
   temp.mkdir('node-sfnt2woff-zopfli', (err, dirPath) => {
     if (err) throw err;
-    const data = fs.readFileSync(path.join(basePath, 'fontawesome-webfont.ttf'));
+    const data = fs.readFileSync(
+      path.join(basePath, 'fontawesome-webfont.ttf')
+    );
     const file = path.join(dirPath, 'encoded.woff');
     // eslint-disable-next-line no-shadow
-    fs.writeFile(file, woff.encode(data), (err) => {
+    fs.writeFile(file, woff.encode(data), err => {
       if (err) throw err;
       const buffer = new Buffer(4);
       // eslint-disable-next-line no-shadow
-      fs.open(file, 'r', (err, fd) => { // eslint-disable-next-line no-shadow
+      fs.open(file, 'r', (err, fd) => {
+        // eslint-disable-next-line no-shadow
         fs.read(fd, buffer, 0, 4, 0, (err, bytesRead, buffer) => {
-          fs.close(fd, (err) => { // eslint-disable-line no-shadow
+          fs.close(fd, err => {
+            // eslint-disable-line no-shadow
             if (err) throw err;
             t.ok(buffer.equals(new Buffer(magic.woff)));
             t.end();
